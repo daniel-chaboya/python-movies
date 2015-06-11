6eeb97b7fdaa37ece4f1d425ee2d991ff413e2ad
Daniel Chaboya <daniel.chaboya@gmail.com>
2015-06-11 11:34:45 -0700

    style: Simple format adjustments Added comments and reformated some of the code to adhere to PEP8 standards.

--- media.py
+++ media.py
@@ -1,7 +1,18 @@
+'''
+This module will serves as a constructor for creating instances of movies.
+Also contains a method that opens a web browser to view the movie trailers.
+'''
+
 import webbrowser
 
+
 class Movie():
-    def __init__(self, movie_title, movie_director, movie_year, movie_storyline, poster_image, trailer_youtube, movie_youtube):
+    """Build an instance of a movie by calling media.Movie()"""
+
+    def __init__(self, movie_title, movie_director,
+                 movie_year, movie_storyline, poster_image,
+                 trailer_youtube, movie_youtube):
+
         self.title = movie_title
         self.director = movie_director
         self.movie_year = movie_year
@@ -10,5 +21,6 @@ class Movie():
         self.trailer_youtube_url = trailer_youtube
         self.movie_youtube_url = movie_youtube
 
+    #open web browser to view youtube movie trailer
     def play_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
